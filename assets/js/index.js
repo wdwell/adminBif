ee452bcf2e3dc903ce7dcb710ec1435e2642b205
wdwell <mailwdwell@yandex.ru>
2023-06-15 00:13:59 +0600

    15.06

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,14 +7,12 @@ document.addEventListener("DOMContentLoaded", function () {
     ".modalAutentificationExit"
   );
   const modalAutentification = document.querySelector(".modalAutentification");
-
   jsEnableModal.forEach((element) => {
     element.addEventListener("click", () => {
       modalAutentificationBlur.style.display = "block";
       modalAutentification.style.display = "block";
     });
   });
-
   modalAutentificationBlur.addEventListener("click", () => {
     modalAutentificationBlur.style.display = "none";
     modalAutentification.style.display = "none";
@@ -23,4 +21,43 @@ document.addEventListener("DOMContentLoaded", function () {
     modalAutentificationBlur.style.display = "none";
     modalAutentification.style.display = "none";
   });
+
+  //проверка валидности email, пароля
+  const emailInput = document.getElementById("emailInput");
+  const emailError = document.getElementById("emailError");
+  const passwordInput = document.getElementById("passwordInput");
+  const passwordError = document.getElementById("passwordError");
+  const submitButton = document.getElementById("submitButton");
+
+  submitButton.addEventListener("click", validateForm);
+  emailInput.addEventListener("click", hideEmailError);
+  passwordInput.addEventListener("click", hidePasswordError);
+
+  function validateForm() {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/;
+    const passwordLength = password.length;
+
+    if (emailPattern.test(email)) {
+      emailError.style.opacity = "0";
+    } else {
+      emailError.style.opacity = "1";
+    }
+
+    if (passwordPattern.test(password) && passwordLength >= 6) {
+      passwordError.style.opacity = "0";
+    } else {
+      passwordError.style.opacity = "1";
+    }
+  }
+
+  function hideEmailError() {
+    emailError.style.opacity = "0";
+  }
+
+  function hidePasswordError() {
+    passwordError.style.opacity = "0";
+  }
 });
--- home.php
+++ home.php
@@ -21,7 +21,33 @@ Template Name: Main Page
                 <img src="<?php bloginfo ('template_url'); ?>/assets/images/exit.svg" alt="exit" />
             </div>
             <div class='modalContent'>
-                продам гараж
+                <form class="autentificationForm">
+                  <h1>Войти в личный кабинет</h1>
+                  <input type="email" id="emailInput" class="custom-input" placeholder="Введите Ваш Email">
+                    <p id="emailError" class="error-message">Вы ввели неверный e-mail</p>
+                    <input type="text" id="passwordInput" class="custom-input" placeholder="Введите Ваш пароль">
+                    <p id="passwordError" class="error-message">Пароль должен содержать строчные и заглавные буквы,<br> не менее 1 цифры, и не должен содержать символы.</p>
+                  <div class="autentificationButton">
+                    <button id="submitButton" class="buttonPrimary button" type="button">
+                      Войти
+                    </button>
+                    <button class="buttonSecondary button" type="button">
+                      Зарегистрироваться
+                    </button>
+                    <button class="buttonLink button" type="button">Восстановить пароль</button>
+                  </div>
+                  
+                  <p class="">или зайдите с помощью</p>
+                    <div class="">
+                        <a href="#"><img src="<?php bloginfo ('template_url'); ?>/assets/images/ok.svg" alt="ok.ru"/></a>
+                        <a href="#"><img src="<?php bloginfo ('template_url'); ?>/assets/images/vk.svg" alt="vk.com" /></a>
+                    </div>
+                    <p class="">
+                    Нажимая на кнопку, вы соглашаетесь с нашими
+                    <a href="#">Условиями</a> и
+                    <a href="#">Политикой конфиденциальности</a>.
+                    </p>
+                </form>
             </div>
         </div>
     </div>
